d1c486f045e0318d688daa0e0a57f61a2ca8429c
bonanashelby <bonanashelby@gmail.com>
2017-05-26 17:56:28 -0700

    finished our queue exercise.

--- README.md
+++ README.md
@@ -2,3 +2,21 @@
 Data Structures for Python 401 
 
 #Pair_programmed with Kurt Maurer and Anna Shelby 
+
+
+##Linked List Branch
+
+
+
+##Stack Branch 
+
+
+
+##DLL Branch
+
+
+
+##Queue Branch 
+#Implement a enqueue(), dequeue(), peek(), size() function for this exercise. 
+#Worked on enqueue and dequeue with our double linked list methods.
+#Used the shift and pop methods from our double linked list in dll.py.
--- src/stack.py
+++ src/stack.py
@@ -9,16 +9,16 @@ class Stack(object):
 
     def __init__(self, data=None):
         """Init for stack."""
-        self._newLinkedList = LinkedList(data)
+        self._new_linked_list = LinkedList(data)
 
     def push(self, val):
         """Push function for stack."""
-        return self._newLinkedList.push(val)
+        return self._new_linked_list.push(val)
 
     def pop(self):
         """Pop function for stack."""
-        return self._newLinkedList.pop()
+        return self._new_linked_list.pop()
 
     def __len__(self):
         """Length function for stack."""
-        return len(self._newLinkedList)
+        return len(self._new_linked_list)

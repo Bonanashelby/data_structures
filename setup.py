590d71b53d3b843f7ce531dc24fd1bbecbb4203c
Kurt <kurtrm@gmail.com>
2017-05-27 09:59:09 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,23 @@
 """Create a Python distribution package setup.py."""
 from setuptools import setup
 
+
+extra_packages = {
+    'testing': ['pytest', 'pytest-watch', 'pytest-cov']
+}
+
+
 setup(
-    name="trigrams",
-    description="A project consisting of several"
-    "Python implementations of abstract data structures.",
+    name="dll",
+    description="Implements the data structure assignment",
     version=0.1,
-    author="Anna and Kurt",
-    license='MIT',
-    py_modules=['dll', 'stack', 'linked_list'],
+    author="Kurt Maurer" "Anna Shelby",
+    author_email="bonanashelby@gmail.com",
+    license="MIT",
+    py_modules=["server, client"],
     package_dir={'': 'src'},
-    extras_require={'test': ['pytest', 'pytest-watch']},
+    install_requires=["ipython", "pytest"],
+    extras_require=extra_packages,
     entry_points={
         'console_scripts': [
         ]
